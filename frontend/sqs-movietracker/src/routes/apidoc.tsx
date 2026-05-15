d65fb5ece2ea694593e4e9f582d282f692539d36
studschmda1533 <daniel.schmerber@stud.th-rosenheim.de>
2026-05-15 15:36:53 +0200

    openapi doc with swagger

--- frontend/sqs-movietracker/src/routes/apidoc.tsx
+++ frontend/sqs-movietracker/src/routes/apidoc.tsx
@@ -1,9 +1,18 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { ClientOnly, createFileRoute } from '@tanstack/react-router'
+import SwaggerUI from 'swagger-ui-react'
+import 'swagger-ui-react/swagger-ui.css'
 
 export const Route = createFileRoute('/apidoc')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/api"!</div>
+  return (
+    <ClientOnly>
+      <SwaggerUI
+        url="/api/openapi.json"
+        tryItOutEnabled={false}
+      />
+    </ClientOnly>
+  )
 }
