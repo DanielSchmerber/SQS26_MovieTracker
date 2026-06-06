c2a98871c5d5ec77d4400d229c31ec88e483d76a
studschmda1533 <daniel.schmerber@stud.th-rosenheim.de>
2026-06-06 15:29:52 +0200

    Initialize readthedocs

---
+++ docs/conf.py
@@ -0,0 +1,17 @@
+project = "SQS MovieTracker"
+author = "SQS26 Team"
+release = "1.0"
+
+extensions = [
+    "myst_parser",
+]
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = []
+
+myst_enable_extensions = ["colon_fence"]
