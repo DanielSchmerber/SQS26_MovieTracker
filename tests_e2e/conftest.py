bf697dc49d9856f6dae8e579159ebd61d856a9d7
Paula Macedo <91981907+macedopaula@users.noreply.github.com>
2026-06-20 18:43:46 +0200

    solve issue with test add to watchlist

--- tests_e2e/conftest.py
+++ tests_e2e/conftest.py
@@ -1,7 +1,8 @@
+import os
 import time
 import pytest
-import os
-
+from playwright.sync_api import Page
+from playwright.sync_api import expect
 
 BASE_URL = os.getenv("E2E_BASE_URL", "https://localhost")
 
@@ -20,36 +21,38 @@ def base_url():
 
 
 @pytest.fixture(scope="session")
+def browser_context_args(browser_context_args):
+    return {
+        **browser_context_args,
+        "ignore_https_errors": True,
+    }
+
+
+@pytest.fixture(scope="session")
 def test_user():
-    timestamp = int(time.time())
+    timestamp = int(time.time()) * 11
 
     return {
         "username": f"test_{timestamp}",
         "email": f"test_{timestamp}@test.com",
-        "password": "Password123!"
+        "password": "Password123!test"
     }
 
 
-
 @pytest.fixture(scope="session")
-def registered_user(browser, base_url, test_user):
+def registered_user(browser, test_user):
     context = browser.new_context(
         ignore_https_errors=True
     )
 
     page = context.new_page()
-
-    page.goto(f"{base_url}/register")
+    page.goto(f"{BASE_URL}/register")
 
     page.get_by_role("textbox", name="Username", exact=True).fill(test_user["username"])
     page.get_by_role("textbox", name="Email", exact=True).fill(test_user["email"])
     page.get_by_role("textbox", name="Password", exact=True).fill(test_user["password"])
     page.get_by_role("textbox", name="Confirm password", exact=True).fill(test_user["password"])
-
-    page.get_by_role(
-        "button",
-        name="Create account"
-    ).click()
+    page.get_by_role("button", name="Create account" ).click()
 
     page.close()
     context.close()
@@ -57,55 +60,3 @@ def registered_user(browser, base_url, test_user):
     return test_user
 
 
-@pytest.fixture()
-def authenticated_page(browser, base_url, registered_user):
-    context = browser.new_context(
-        ignore_https_errors=True
-    )
-
-    page = context.new_page()
-
-    page.goto(f"{base_url}/login")
-
-    page.get_by_role("textbox", name="Username", exact=True).fill(registered_user["username"])
-   
-    page.get_by_role("textbox", name="Password", exact=True).fill(registered_user["password"])
-    
-
-    page.get_by_role(
-        "button",
-        name="Sign in"
-    ).click()
-
-    return page
-
-
-@pytest.fixture()
-def authenticated_xss_page(browser, base_url):
-    timestamp = int(time.time() * 1000)
-
-    username = f"xss_{timestamp}"
-    password = "Password123!"
-
-    context = browser.new_context(
-        ignore_https_errors=True
-    )
-
-    page = context.new_page()
-
-    # register
-    page.goto(f"{base_url}/register")
-
-    page.get_by_role("textbox", name="Username", exact=True).fill(username)
-    page.get_by_role("textbox", name="Email", exact=True).fill(f"{username}@test.com")
-    page.get_by_role("textbox", name="Password", exact=True).fill(password)
-    page.get_by_role("textbox", name="Confirm password", exact=True).fill(password)
-
-    page.get_by_role(
-        "button",
-        name="Create account"
-    ).click()
-
-    yield page
-
-    context.close()
--- tests_e2e/test_app.py
+++ tests_e2e/test_app.py
@@ -1,330 +1,178 @@
-import time
 import re
 from playwright.sync_api import expect
 
+
 MOVIE_NAME = "Dirty Dancing"
 
-INVALID_USERNAME = "invalid_user"
-INVALID_PASSWORD = "wrong_password"
+INVALID_USERNAME = "invalid_user123"
+INVALID_PASSWORD = "wrong_password123"
 
 MOVIE_ID = 88
+MOVIE_ID1 = 22
 
 
-def login(page, base_url, username, password):
+
+def login(page, username, password,  base_url):
     page.goto(f"{base_url}/login")
 
     page.get_by_role("textbox", name="Username", exact=True).fill(username)
     page.get_by_role("textbox", name="Password", exact=True).fill(password)
-
-    page.get_by_role(
-        "button",
-        name="Sign in"
-    ).click()
-   
+    page.get_by_role("button", name="Sign in").click()
 
 
-def test_register_page_loads_t01(page, base_url):
+def test_register_page_loads(page,  base_url):
     page.goto(f"{base_url}/register")
+    expect(page.locator('div[data-slot="card-title"]')).to_have_text("Create an account")
 
-    expect(
-        page.locator('div[data-slot="card-title"]')).to_have_text("Create an account")
-    
 
-def test_register_new_user_t02(page, base_url):
-    username = f"test_user_{int(time.time())}"
-    email = f"{username}@test.com"
-
+def test_register_new_user(page, test_user,  base_url):
+    username = test_user["username"]
+    email = test_user["email"]
+    password = test_user["password"]
     page.goto(f"{base_url}/register")
-
     page.get_by_label("Username").fill(username)
     page.get_by_label("Email").fill(email)
-
-   
-
-    page.get_by_role("textbox", name="Password", exact=True).fill("Password123!")
-
-    page.get_by_role(
-    "textbox",
-    name="Confirm password",
-    exact=True ).fill("Password123!")
-
-    page.get_by_role(
-        "button",
-        name="Create account"
-    ).click()
-
-    expect(page).to_have_url(
-        re.compile(".*/$")
-    )
+    page.get_by_role("textbox", name="Password", exact=True).fill(password)
+    page.get_by_role("textbox",name="Confirm password", exact=True ).fill(password)
+    page.get_by_role("button", name="Create account").click()
+    expect(page).to_have_url(re.compile(".*/$") )
 
 
-def test_register_password_mismatch_t03(page, base_url):
+def test_register_password_mismatch(page, test_user,  base_url):
+    username = test_user["username"]
+    email = test_user["email"]
+    password = test_user["password"]
     page.goto(f"{base_url}/register")
-
-    page.get_by_label("Username").fill("user")
-    page.get_by_label("Email").fill("user@test.com")
-
-    page.get_by_role("textbox", name="Password", exact=True).fill("Password123!")
-
-    page.get_by_role(
-    "textbox",
-    name="Confirm password",
-    exact=True ).fill("Password123!wrong")
-
-    page.get_by_role(
-        "button",
-        name="Create account"
-    ).click()
-
-    expect(
-        page.locator(".text-destructive")
-    ).to_be_visible()
+    page.get_by_label("Username").fill(username)
+    page.get_by_label("Email").fill(email)
+    page.get_by_role("textbox", name="Password", exact=True).fill(password)
+    page.get_by_role("textbox", name="Confirm password", exact=True).fill("Password123!wrong")
+    page.get_by_role("button", name="Create account").click()
+    expect(page.locator(".text-sm.text-destructive")).to_be_visible()
 
 
-def test_register_link_to_login_t04(page, base_url):
+def test_register_link_to_login(page,  base_url):
     page.goto(f"{base_url}/register")
+    page.get_by_role("link", name="Sign in").click()
+    expect(page).to_have_url(re.compile(".*/login"))
 
-    page.get_by_role(
-        "link",
-        name="Sign in"
-    ).click()
-
-    expect(page).to_have_url(
-        re.compile(".*/login")
-    )
-
-
-def test_invalid_movie_displays_error_t05(page, base_url):
-    page.goto(
-        f"{base_url}/movie/6666666666"
-    )
-
-    expect(
-        page.get_by_text("Oops!")
-    ).to_be_visible(timeout=50_000)
-
-
-def test_login_page_loads_t06(page, base_url):
+def test_login_page_loads(page,  base_url):
     page.goto(f"{base_url}/login")
-
-    expect(
-        page.locator('div[data-slot="card-title"]')).to_have_text("Welcome back")
-    
+    expect(page.locator('div[data-slot="card-title"]')).to_have_text("Welcome back")
+    expect(page.get_by_role("button", name="Sign in")).to_be_visible()
 
 
-def test_login_valid_credentials_t07(page, base_url, registered_user):
+def test_login_invalid_password(page, registered_user):
     login(
         page,
-        base_url,
+        registered_user["username"],
+        INVALID_PASSWORD
+    )
+    expect(page.locator(".text-sm.text-destructive")).to_be_visible(timeout=10000)
+
+
+def test_login_invalid_username(page, registered_user):
+    login(
+        page,
+        INVALID_USERNAME,
+        registered_user["password"]
+    )
+    expect(page.locator(".text-sm.text-destructive")).to_be_visible(timeout=10000)
+
+
+def test_login_invalid_username_invalid_password(page):
+    login(
+        page,
+        INVALID_USERNAME,
+        INVALID_PASSWORD
+    )
+    expect(page.locator(".text-sm.text-destructive")).to_be_visible(timeout=10000)
+
+def test_login_valid_credentials(page, registered_user):
+    login(
+        page,
         registered_user["username"],
         registered_user["password"]
     )
 
-    expect(page).to_have_url(
-        re.compile(".*/$")
-    )
+    expect(page).to_have_url(re.compile(".*/$"))
+    expect(page.get_by_role("button", name="Sign out")).to_be_visible()
 
+### Non authenticated
 
-def test_login_invalid_password_t08(page, base_url, registered_user):
-    login(
-        page,
-        base_url,
-        registered_user["username"],
-        INVALID_PASSWORD
-    )
-
-    expect(
-        page.locator(".text-destructive")
-    ).to_be_visible()
-
-
-def test_login_invalid_username_t09(page, base_url, registered_user):
-    login(
-        page,
-        base_url,
-        INVALID_USERNAME,
-        registered_user["password"]
-    )
-
-    expect(
-        page.locator(".text-destructive")
-    ).to_be_visible()
-
-
-def test_login_invalid_username_invalid_password_t10(page, base_url):
-    login(
-        page,
-        base_url,
-        INVALID_USERNAME,
-        INVALID_PASSWORD
-    )
-
-    expect(
-        page.locator(".text-destructive")
-    ).to_be_visible()
-
-
-
-
-def test_search_page_without_query_t11(page, base_url):
+def test_search_page_without_query(page,  base_url):
     page.goto(f"{base_url}/search")
-
-    expect(
-        page.get_by_text(
-            "No Search results yet. Start searching for Movies"
-        )
-    ).to_be_visible()
+    expect(page.get_by_text("No Search results yet. Start searching for Movies")).to_be_visible()
 
 
-def test_search_movie_t12(page, base_url):
+
+def test_search_movie(page,  base_url):
     page.goto(base_url)
 
-    page.get_by_placeholder(
-        "Search for a movie…"
-    ).fill(MOVIE_NAME)
+    page.get_by_placeholder("Search for a movie…").fill(MOVIE_NAME)
+    page.get_by_role("button", name="Search", exact=True).click()
+    expect(page).to_have_url(re.compile(".*search.*"))
+    page.goto(f"{base_url}/search?title={MOVIE_NAME}")
+    expect(page.locator("button").first).to_be_visible()
+    page.get_by_role("button").filter(has_text= MOVIE_NAME).first.click()
+    expect(page).to_have_url(re.compile(".*/movie/.*"))
 
-    page.get_by_role(
-        "button",
-        name="Search"
-    ).click()
 
-    expect(page).to_have_url(
-        re.compile(".*search.*")
+def test_movie_details_page_loads(page,  base_url):
+    page.goto(f"{base_url}/movie/{MOVIE_ID}")
+    expect(page.get_by_role("heading").first).to_be_visible()
+    expect(page.locator("p").first).to_be_visible()
+    expect(page.locator("img[alt]").first).to_be_visible()
+    expect(page.get_by_text("TMDB")).to_be_visible()
+
+
+def test_loads_watchlist(page,  base_url):
+    page.goto(f"{base_url}/watchlist" )
+    expect(page.get_by_text("You need to be logged in to view your watchlist." )).to_be_visible()
+
+## Authenticated
+
+def test_add_watchlist(page,  base_url):
+    page.goto(f"{base_url}/watchlist" )
+    expect(page.get_by_text("You need to be logged in to view your watchlist." )).to_be_visible()
+
+
+
+def test_add_movie_to_watchlist(page, registered_user,  base_url):
+    login(
+        page, 
+        registered_user["username"],
+        registered_user["password"]
     )
+    expect(page.get_by_role("button", name="Sign out")).to_be_visible(timeout=10000)
 
-
-def test_search_results_are_displayed_t13(page, base_url):
-    page.goto(
-        f"{base_url}/search?title={MOVIE_NAME}"
-    )
-
-    expect(
-        page.locator("button").first
-    ).to_be_visible()
-
-
-def test_movie_result_can_be_opened_t14(page, base_url):
-    page.goto(
-        f"{base_url}/search?title={MOVIE_NAME}"
-    )
-
-    page.get_by_role("button").filter(
-        has_text= MOVIE_NAME
-    ).first.click()
-
-    expect(page).to_have_url(
-        re.compile(".*/movie/.*")
-    )
-
-
-
-def test_movie_details_page_loads_t15(page, base_url):
-    page.goto(
-        f"{base_url}/movie/{MOVIE_ID}"
-    )
-
-    expect(
-        page.get_by_role("heading").first
-    ).to_be_visible()
-
-
-def test_movie_description_displayed_t16(page, base_url):
-    page.goto(
-        f"{base_url}/movie/{MOVIE_ID}"
-    )
-
-    expect(
-        page.locator("p").first
-    ).to_be_visible()
-
-
-def test_movie_poster_displayed_t17(page, base_url):
-    page.goto(
-        f"{base_url}/movie/{MOVIE_ID}"
-    )
-
-    expect(
-        page.locator("img[alt]").first
-    ).to_be_visible()
-
-
-def test_tmdb_rating_displayed_t18(page, base_url):
-    page.goto(
-        f"{base_url}/movie/{MOVIE_ID}"
-    )
-
-    expect(
-        page.get_by_text("TMDB")
-    ).to_be_visible()
-
-
-
-def test_watchlist_requires_login_t19(page, base_url):
-    page.goto(
-        f"{base_url}/watchlist"
-    )
-
-    expect(
-        page.get_by_text(
-            "You need to be logged in to view your watchlist."
-        )
-    ).to_be_visible()
-
-
-def test_add_movie_to_watchlist_t20(authenticated_page, base_url):
-    page = authenticated_page
-
-    page.goto(
-        f"{base_url}/movie/{MOVIE_ID}"
-    )
-
-    add_button = page.get_by_role(
-        "button",
-        name="Add to Watchlist"
-    )
-
-    expect(add_button).to_be_enabled(timeout=20000)
-
+    page.goto(f"{base_url}/movie/{MOVIE_ID1}")
+    add_button = page.get_by_role("button", name="Add to Watchlist", exact=True)
+    expect(add_button).to_be_enabled(timeout=50000)
     add_button.click()
+    expect(page.get_by_role("button", name="Watched")).to_be_visible(timeout=10000)
 
-    expect(
-        page.get_by_role(
-            "button",
-            name="Watched"
-        )
-    ).to_be_visible(timeout=10000)
+def test_remove_movie_from_watchlist(page, registered_user,  base_url):
+    login(
+        page, 
+        registered_user["username"],
+        registered_user["password"]
+    )
+    expect(page.get_by_role("button", name="Sign out")).to_be_visible(timeout=10000)
 
+    page.goto(f"{base_url}/movie/{MOVIE_ID1}")
 
-def test_remove_movie_from_watchlist_t21(authenticated_page, base_url):
-    page = authenticated_page
+    watched_button = page.get_by_role("button", name="Watched",  exact=True)
 
-    page.goto(
-        f"{base_url}/movie/{MOVIE_ID}"
+    expect(watched_button).to_be_visible(timeout=10000)
+
+    watched_button.click()
+  
+    add_button = page.get_by_role(
+    "button",
+    name="Add to Watchlist",
+    exact=True
     )
 
-    expect(
-        page.get_by_role(
-            "button",
-            name="Watched"
-        )
-    ).to_be_visible(timeout=10000)
-
-    watched_button = page.get_by_role(
-        "button",
-        name="Watched"
-    )
-
-    if watched_button.count() > 0:
-        watched_button.click()
-
-    expect(
-        page.get_by_role(
-            "button",
-            name="Add to Watchlist"
-        )
-    ).to_be_visible()
-
-
-
-
-
+    expect(add_button).to_be_visible(timeout=10000)
+    expect(add_button).to_be_enabled(timeout=10000)
