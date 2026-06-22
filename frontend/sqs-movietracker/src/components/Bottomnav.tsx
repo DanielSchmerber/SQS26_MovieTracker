56c4bbf39519e2d954b54389e2e8a09541850365
studschmda1533 <daniel.schmerber@stud.th-rosenheim.de>
2026-06-22 17:27:37 +0200

    Removed layout-placeholder with actual content, fixed sonarqube css-bug

--- frontend/sqs-movietracker/src/components/Bottomnav.tsx
+++ frontend/sqs-movietracker/src/components/Bottomnav.tsx
@@ -1,14 +1,21 @@
 // components/bottom-nav.tsx
 
 import { Link } from "@tanstack/react-router";
+import { GitBranch  } from 'lucide-react'
 
 export function BottomNav() {
   return (
     <footer className="mt-auto border-t border-black/10 bg-white/60 backdrop-blur-xl dark:border-white/10 dark:bg-black/50">
       <div className="mx-auto flex flex-col items-center justify-between gap-4 px-6 py-5 text-xs text-neutral-500 md:max-w-7xl md:flex-row">
-        <div className="font-semibold tracking-wide">
-          PLACEHOLDER
-        </div>
+        <a
+          href="https://github.com/DanielSchmerber/SQS26_MovieTracker"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-flex items-center gap-1 transition hover:text-black dark:hover:text-white"
+        >
+          <GitBranch size={16} />
+          Repository
+        </a>
 
         <div>© 2026 Movietracker</div>
 
